11b6626a67dcdf9d122d32d5ce9fb0fecf90c083
Jean Ferri <jeanferrimh@gmail.com>
2023-03-03 13:24:03 -0300

    fix problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,15 @@ document.onload = financeController.showFinanceLocalStorage()
 
 console.log(financeController._earningList._earningList)
 
-let buttonRemoveEarn = new Array(document.querySelectorAll("#buttonRemoveEarn"))
+let buttonRemoveEarn = document.querySelectorAll("#buttonRemoveEarn")
 
-buttonRemoveEarn.forEach(item => item.addEventListener('click', () => console.log(item)))
+let buttonRemoveEarnArray = []
+
+buttonRemoveEarn.forEach(item =>  buttonRemoveEarnArray.push(item)) 
+
+buttonRemoveEarnArray.map(item => item.addEventListener('click', financeController.itemRemoveEarn(buttonRemoveEarnArray.indexOf(item))))
+
+console.log(buttonRemoveEarnArray)
 
 
 
